3ff5476729561f25e3790ab019a3b27054aacad1
Katerina Hanusova <k.hanusova90@gmail.com>
2016-12-20 13:56:24 +0100

    UPDATE (#622397): Make icons clickable

--- app/src/main/java/cz/hanusova/fingerprint_game/MapActivity.java
+++ app/src/main/java/cz/hanusova/fingerprint_game/MapActivity.java
@@ -2,18 +2,10 @@ package cz.hanusova.fingerprint_game;
 
 import android.annotation.TargetApi;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.graphics.Rect;
-import android.graphics.Region;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
-import android.os.AsyncTask;
 import android.os.Build;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Bean;
@@ -23,7 +15,6 @@ import org.androidannotations.annotations.OnActivityResult;
 import org.androidannotations.annotations.ViewById;
 import org.androidannotations.annotations.res.DrawableRes;
 
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -47,6 +38,8 @@ public class MapActivity extends AbstractAsyncActivity {
 
     private static final int REQ_CODE_QR = 1;
     private static final int ICON_SIZE = 32;
+    private static final int MAP_HEIGHT = 2800;
+    private static final int MAP_WIDTH = 2600;
 
     @Bean(UserServiceImpl.class)
     UserService userService;
@@ -77,15 +70,13 @@ public class MapActivity extends AbstractAsyncActivity {
     void init(){
         try {
             Bitmap bitmap = new cz.hanusova.fingerprint_game.task.MapAsyncTask().execute("J1NP.jpg").get();
-            Drawable mapDrawable = new BitmapDrawable(getResources(), bitmap);
-
+//            Drawable mapDrawable = new BitmapDrawable(getResources(), bitmap);
+            Drawable mapDrawable = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(bitmap, MAP_WIDTH, MAP_HEIGHT, true));
             AppUser user = userService.getActualUser();
             List<Drawable> icons = getIcons(user.getPlaces());
 
             LayerDrawable layers = createLayers(mapDrawable, icons);
             changeIconPosition(mapDrawable, user.getPlaces(), layers);
-
-            //TODO: pridat onTouchListener
         } catch (InterruptedException e) {
             e.printStackTrace(); //TODO: handle exceptions
         } catch (ExecutionException e) {
@@ -114,7 +105,6 @@ public class MapActivity extends AbstractAsyncActivity {
     }
 
     private LayerDrawable createLayers(Drawable mapDrawable, List<Drawable> icons) {
-//        LayerDrawable ld = new LayerDrawable(new Drawable[]{map});
         Drawable[] layers = new Drawable[icons.size() + 1];
         layers[0] = mapDrawable;
         for (int i = 0; i < icons.size(); i++) {
@@ -122,7 +112,6 @@ public class MapActivity extends AbstractAsyncActivity {
             layers[i + 1] = icon;
         }
         return new LayerDrawable(layers);
-//        mapView.setImageDrawable(ld);
     }
 
     private void changeIconPosition(Drawable mapDrawable, List<Place> places, LayerDrawable ld) {
@@ -138,74 +127,6 @@ public class MapActivity extends AbstractAsyncActivity {
         mapView.setImageDrawable(ld);
     }
 
-    private void showMap() {
-        Bitmap bitmap = ((BitmapDrawable) icon).getBitmap();
-        Drawable smallIcon = new BitmapDrawable(getResources(), bitmap);
-        //TODO: zkusit nacpat jen icon
-        LayerDrawable iconLd = new LayerDrawable(new Drawable[]{map, smallIcon});
-//        new MapAsyncTask().execute(); //TODO: stahnout ze serveru
-        int width = map.getIntrinsicWidth();
-        int height = map.getIntrinsicHeight();
-
-        int posX = 50;
-        int posY = 130;
-
-        int iconW = 50;
-        int iconH = 50;
-
-        iconLd.setLayerInset(1, posX, posY, width - posX - iconW, height - posY - iconH); //TODO: for cyklus pro index kazde ikony -> od 1 do icons.size
-        mapView.setImageDrawable(iconLd);
-
-        mapView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    float x = event.getX();
-                    float y = event.getY();
-
-                    Matrix m = mapView.getImageMatrix();
-                    float[] values = new float[9];
-                    m.getValues(values);
-                    float scaleX = values[Matrix.MSCALE_X];
-                    float scaleY = values[Matrix.MSCALE_Y];
-
-                    LayerDrawable ld = (LayerDrawable) mapView.getDrawable();
-
-                    for (int i = 0; i < ld.getNumberOfLayers(); i++) {
-                        BitmapDrawable d = (BitmapDrawable) ld.getDrawable(i);
-                        Bitmap b = d.getBitmap();
-                        Region reg = d.getTransparentRegion();
-                        Rect r = d.getBounds();
-                        System.out.println("RECT left: " + r.left);
-                        System.out.println("RECT right: " + r.right);
-                        System.out.println("RECT top: " + r.top);
-                        System.out.println("RECT bot: " + r.bottom);
-
-                        if (r.contains((int) x, (int) y)) {
-                            System.out.println("Icon at " + i + " position was clicked!");
-                            return true;
-                        }
-                    }
-                    System.out.println("NOTHING WAS CLICKED");
-                }
-                return false;
-            }
-        });
-
-//        iv.setImageDrawable(iconLd);
-//        Button btn = new Button(this);
-//        btn.setPadding(posX, posY, width - posX - iconW, height - posY - iconH);
-//        btn.setBackground(icon);
-
-
-//        ImageView image = new ImageView(getApplicationContext());
-//        image.setImageBitmap(bitmap);
-//        Drawable d = image.getDrawable();
-//        LayerDrawable ld = new LayerDrawable(new Drawable[]{map, d});
-//        iv.setImageDrawable(ld);
-    }
-
-
     @Click(R.id.map_camera)
     void startCamera(){
         QrActivity_.intent(this).startForResult(REQ_CODE_QR);
@@ -217,68 +138,4 @@ public class MapActivity extends AbstractAsyncActivity {
         System.out.println("ZOBRAZOVANI AKTIVIT");
     }
 
-    //TODO: externalizovat
-    private class MapAsyncTask extends AsyncTask<Void, Void, Bitmap> {
-
-        @Override
-        protected void onPreExecute() {
-            showLoadingProgressDialog();
-        }
-
-        @Override
-        protected Bitmap doInBackground(Void... params) {
-            Bitmap bitmap = null;
-            try {
-                URL url = new URL(Constants.IMG_URL_BASE + "J1NP.jpg");
-//                URL url = new URL(Constants.IMG_URL_BASE + "mozaika.jpg");
-                bitmap = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-
-                URL iconUrl = new URL(Constants.IMG_URL_BASE + "money_icon.png");
-                Bitmap icon = BitmapFactory.decodeStream(iconUrl.openConnection().getInputStream());
-
-                icons.add(icon);
-            } catch (Exception e) {
-                e.printStackTrace();
-                Log.e(TAG, "Could not download map from server");
-            }
-            return bitmap;
-        }
-
-        @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            dismissProgressDialog();
-            TouchImageView iv = (TouchImageView) findViewById(R.id.img_map);
-            if (bitmap != null) {
-//                iv.setMaxZoom(4f);
-            }
-            addIcons(iv, bitmap);
-        }
-
-        private void addIcons(TouchImageView iv, Bitmap bitmap){
-//            List<ImageView> iconList = new ArrayList<>();
-            List<Drawable> drawables = new ArrayList<>();
-            Drawable mapBitmap = new BitmapDrawable(getResources(), bitmap);
-//            Drawable mapBitmap = getResources().getDrawable(R.drawable.j4np);
-            drawables.add(mapBitmap);
-            for (Bitmap icon : icons){
-                Bitmap smallIcon = Bitmap.createBitmap(icon, 10, 20, 15, 15);
-//                ImageView image = new ImageView(context);
-//                image.setImageBitmap(smallIcon);
-//                iconList.add(image);
-                Drawable drawable = new BitmapDrawable(getResources(), smallIcon);
-                drawables.add(drawable);
-            }
-
-            Drawable drawable = getResources().getDrawable(R.drawable.money_icon);
-            drawables.add(drawable);
-
-            LayerDrawable ld = new LayerDrawable(drawables.toArray(new Drawable[drawables.size()]));
-            ld.setLayerInset(1, 20, 30, 100, 120);
-            iv.setImageDrawable(ld);
-        }
-    }
-
-    public void setIcons(List<Bitmap> icons){
-        this.icons = icons;
-    }
 }
--- app/src/main/java/cz/hanusova/fingerprint_game/view/TouchImageView.java
+++ app/src/main/java/cz/hanusova/fingerprint_game/view/TouchImageView.java
@@ -3,8 +3,12 @@ package cz.hanusova.fingerprint_game.view;
 import android.content.Context;
 import android.graphics.Matrix;
 import android.graphics.PointF;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.LayerDrawable;
 import android.support.v4.view.MotionEventCompat;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
@@ -18,6 +22,7 @@ public class TouchImageView extends ImageView {
     private static final int NONE = 0;
     private static final int DRAG = 1;
     private static final int ZOOM = 2;
+    private static final int TAP = 3;
     private int mode = NONE;
 
     private Matrix matrix;
@@ -25,7 +30,8 @@ public class TouchImageView extends ImageView {
     private PointF lastPoint = new PointF();
     private PointF startPoint = new PointF();
 
-    private ScaleGestureDetector detector;
+    private ScaleGestureDetector scaleDetector;
+    private GestureDetector gestureDetector;
 
     public TouchImageView(Context context) {
         super(context);
@@ -39,7 +45,8 @@ public class TouchImageView extends ImageView {
 
     private void initConstruct(Context context) {
         super.setClickable(true);
-        detector = new ScaleGestureDetector(context, new MyScaleListener());
+        scaleDetector = new ScaleGestureDetector(context, new ScaleListener());
+        gestureDetector = new GestureDetector(context, new TapListener());
         matrix = new Matrix();
         setImageMatrix(matrix);
         setScaleType(ScaleType.MATRIX);
@@ -50,7 +57,8 @@ public class TouchImageView extends ImageView {
         setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                detector.onTouchEvent(event);
+                scaleDetector.onTouchEvent(event);
+                gestureDetector.onTouchEvent(event);
                 int pointerIndex = MotionEventCompat.getActionIndex(event);
                 float currX = MotionEventCompat.getX(event, pointerIndex);
                 float currY = MotionEventCompat.getY(event, pointerIndex);
@@ -58,9 +66,21 @@ public class TouchImageView extends ImageView {
                 int action = MotionEventCompat.getActionMasked(event);
                 switch (action) {
                     case MotionEvent.ACTION_DOWN:
-                        mode = DRAG;
-                        lastPoint.set(currX, currY);
-                        startPoint.set(currX, currY);
+                        if (mode == TAP) {
+                            LayerDrawable ld = (LayerDrawable) getDrawable();
+                            for (int i = 1; i < ld.getNumberOfLayers(); i++) {
+                                BitmapDrawable d = (BitmapDrawable) ld.getDrawable(i);
+                                Rect r = d.getBounds();
+
+                                if (r.contains((int) currX, (int) currY)) {
+                                    System.out.println("Icon at " + i + " position was clicked!");
+                                }
+                            }
+                        } else {
+                            mode = DRAG;
+                            lastPoint.set(currX, currY);
+                            startPoint.set(currX, currY);
+                        }
                         break;
 
                     case MotionEvent.ACTION_MOVE:
@@ -88,7 +108,7 @@ public class TouchImageView extends ImageView {
         });
     }
 
-    private class MyScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
 
         @Override
         public boolean onScaleBegin(ScaleGestureDetector detector) {
@@ -99,9 +119,24 @@ public class TouchImageView extends ImageView {
         @Override
         public boolean onScale(ScaleGestureDetector detector) {
             float scaleFactor = detector.getScaleFactor();
-
             matrix.postScale(scaleFactor, scaleFactor, detector.getFocusX(), detector.getFocusY());
             return true;
+        }
+    }
+
+    private class TapListener extends GestureDetector.SimpleOnGestureListener {
+
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            mode = TAP;
+            return true;
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            mode = DRAG;
+            return true;
+//            return super.onScroll(e1, e2, distanceX, distanceY);
         }
     }
 }
