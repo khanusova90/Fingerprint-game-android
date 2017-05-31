d9075cc589d5d0b64599f6d04a58ff943db9e15e
Katerina Hanusova <k.hanusova90@gmail.com>
2017-05-31 21:47:14 +0200

    #672937: redirected to https

--- app/src/main/java/cz/hanusova/fingerprint_game/utils/Constants.java
+++ app/src/main/java/cz/hanusova/fingerprint_game/utils/Constants.java
@@ -15,8 +15,8 @@ public final class Constants {
 //    public static final String IMG_URL_BASE = "http://10.0.2.2:8080/fingerprint-game/disk-resources/";
 
     //prod
-    public static final String URL_BASE = "http://beacon.uhk.cz/fingerprint-game/android/1.0";
-    public static final String IMG_URL_BASE = "http://beacon.uhk.cz/fingerprint-game/disk-resources/";
+    public static final String URL_BASE = "https://beacon.uhk.cz/fingerprint-game/android/1.0";
+    public static final String IMG_URL_BASE = "https://beacon.uhk.cz/fingerprint-game/disk-resources/";
 
     //Name of app for SharedPreferences
     public static final String SP_NAME = "fingerprint_game";
